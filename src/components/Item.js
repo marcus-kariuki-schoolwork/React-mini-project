9c0b03b3d46576f4a583fbb93daa5f592348f27d
Marcus <marcus.kariuki@student.moringaschool.com>
2023-01-15 22:55:10 +0300

    1.Add onClick eventhandler on the button element to display items in the virtual cart 2.use state hook to show when the item has been added or removed from the virtual cart

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [addToCart, setaddToCart] = useState(false);
+
+  function handleAddToCart(){
+    setaddToCart((addToCart) => !addToCart);
+  }
+
+
   return (
-    <li className="">
+    <li className={addToCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleAddToCart} className="add">{addToCart ? "Remove From Cart" : "Add to Cart"}</button>
     </li>
   );
 }
